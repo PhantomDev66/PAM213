b589f5d64dd3677ee3404669c7454ac2cde5216c
PhantomDev <76745951+PhantomDev66@users.noreply.github.com>
2025-09-09 15:08:55 -0600

    Creación de carpeta jsParaPrincipiantes
    Se creo la carpeta jsParaPrincipiantes
    Se creo un Readme.md
    Se creo un HelloWorld.js

---
+++ jsParaPrincipiantes/helloWolrd.js
@@ -0,0 +1,11 @@
+
+
+// =================================
+
+//  Hello World en Consola de Chrome
+
+// =================================
+
+console.log("Hello World!"); //Mostrar contenido en consola
+
+document.write("Hello World");// Escribir 
