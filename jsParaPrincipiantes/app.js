55529ca3f3159aab86193442a27bb9a74a02e90a
PhantomDev <76745951+PhantomDev66@users.noreply.github.com>
2025-09-10 18:38:16 -0600

    Finalización de practica
    Olvide hacer los commits en el momento, pero cree los archivos de app.js
    
    -Se crea el metodo que imprime en consola llamado Add
    
    -Se usa la función de Add

---
+++ jsParaPrincipiantes/app.js
@@ -0,0 +1,13 @@
+
+//Funcion Add para imprimir en consola numeros
+function add(numero1, numero2){
+    console.log(numero1, numero2);
+}
+
+//Casos de uso
+add(3,2);
+add(100,100);
+
+add(3,2);
+add(100,100);
+add(3123123123,132123123);
